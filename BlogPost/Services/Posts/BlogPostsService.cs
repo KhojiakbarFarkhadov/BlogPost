f78a03e4997f16eec45d383b8dc3c855fc4703b7
Khojiakbar <f.f.xojiakbar@gmail.com>
2022-06-10 16:59:47 +0500

    #18 : Updated GetApprovedById method of BlogPostsService.cs file

--- BlogPost/Services/Posts/BlogPostsService.cs
+++ BlogPost/Services/Posts/BlogPostsService.cs
@@ -17,11 +17,20 @@ namespace BlogPost.Services.Posts
 
         public Post GetApprovedById(int id)
         {
-            var post = _context.Posts
-            .Where(m => m.Status == "Approved")
-            .FirstOrDefault(m => m.Id == id);
+            var post = GetById(id);
 
+            if (post != null && !IsApproved(post))
+            {
+                post = null;
+            }
+            
             return post;
+        }
+
+        private bool IsApproved(Post post)
+        {
+            bool isApproved = post.Status == "Approved";
+            return isApproved;
         }
     }
 }
